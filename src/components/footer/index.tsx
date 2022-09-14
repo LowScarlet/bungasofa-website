2500296b23438c6f5330bc2fdf88b6f142ed4f18
Low_Scarlet <namaetohyaakiradesu@gmail.com>
2022-09-14 19:02:30 +0700

    !

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,25 +1,65 @@
+/* eslint-disable react/no-unescaped-entities */
 function main() {
   return (<>
-    <footer className="bg-dark text-center text-white">
-      <div className="container p-4 pb-0">
-        <section className="mb-4">
-          <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-          ><i className="bi bi-whatsapp"/>
-          </a>
-
-          <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-          ><i className="bi bi-instagram"/>
-          </a>
-
-          <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-          ><i className="bi bi-telephone-fill"/>
-          </a>
-        </section>
+  <div className="text-bg-dark py-5">
+  <footer className="container">
+    <div className="row">
+      <div className="col-6 col-md-2 mb-3">
+        <h5>Section</h5>
+        <ul className="nav flex-column">
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+        </ul>
       </div>
-      <div className="text-center p-3">
-        © 2022 Copyright <a className="text-white" href="https://bungasofa.id/">www.bungasofa.id</a>
+
+      <div className="col-6 col-md-2 mb-3">
+        <h5>Section</h5>
+        <ul className="nav flex-column">
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+        </ul>
       </div>
-    </footer>
+
+      <div className="col-6 col-md-2 mb-3">
+        <h5>Section</h5>
+        <ul className="nav flex-column">
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+        </ul>
+      </div>
+
+      <div className="col-md-5 offset-md-1 mb-3">
+        <form>
+          <h5>Subscribe to our newsletter</h5>
+          <p>Monthly digest of what's new and exciting from us.</p>
+          <div className="d-flex flex-column flex-sm-row w-100 gap-2">
+            <label htmlFor="newsletter1" className="visually-hidden">Email address</label>
+            <input id="newsletter1" type="text" className="form-control" placeholder="Email address"/>
+            <button className="btn btn-primary" type="button">Subscribe</button>
+          </div>
+        </form>
+      </div>
+    </div>
+
+    <div className="d-flex flex-column flex-sm-row justify-content-between pt-4 mt-4 border-top">
+      <p>&copy; 2022 Company, Inc. All rights reserved.</p>
+      <ul className="list-unstyled d-flex">
+        {/* <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"><use xlink:href="#twitter"/></svg></a></li>
+        <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
+        <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"><use xlink:href="#facebook"/></svg></a></li> */}
+      </ul>
+    </div>
+  </footer>
+  </div>
   </>)
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,104 +1,41 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 // import type { NextPage } from 'next'
-import { Button } from 'reactstrap'
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap'
 import Layout from '../components/layout'
 import Image from 'next/image'
 import styles from './style.module.css'
 import Link from 'next/link'
 import Marquee from "react-fast-marquee";
+import Head from 'next/head'
+import { useState } from 'react'
 
 function Main() {
-  return (
+  const [modal, setModal] = useState(false);
+  const [backdrop, setBackdrop] = useState(true);
+  const [keyboard, setKeyboard] = useState(true);
+
+  const toggle = () => setModal(!modal);
+
+  const changeBackdrop = (e:any) => {
+    let { value } = e.target;
+    if (value !== 'static') {
+      value = JSON.parse(value);
+    }
+    setBackdrop(value);
+  };
+
+  const changeKeyboard = (e:any) => {
+    setKeyboard(e.currentTarget.checked);
+  };
+  
+  return (<>
+    <Head>
+      <title>Bunga Sofa - Servis & Produsen Sofa Riau</title>
+    </Head>
     <Layout>
-      <div id="myCarousel" className={`${styles['carousel']} carousel slide mb-0`} data-bs-ride="carousel">
-        <div className="carousel-indicators">
-          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div className="carousel-inner">
-          <div className={`${styles['carousel-item']} carousel-item active`}>
-            <img className={styles['bd-placeholder-img']} width="100%" height="100%" src="/static/images/heroes/main.png" />
-
-            <div className={`${styles['carousel-caption']} carousel-caption text-start`}>
-              <div className='container'>
-                <Image className="d-block mx-auto" src="/static/logo/logo_with_brandname.png" alt="" width={236} height={64} />
-                <p>
-                Tips Cerdas Yang Membuat Rumah Anda Menjadi Lebih Mewah dan Berkelas
-                </p>
-                <div className="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
-                  <Link href="/account">
-                    <a className="btn btn-primary px-4 me-sm-3">
-                      Tentang Kami
-                    </a>
-                  </Link>
-                  <Link href="/community">
-                    <a className="btn btn-outline-light px-4">
-                      Lihat Artikel
-                    </a>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={`${styles['carousel-item']} carousel-item`}>
-            <img className={styles['bd-placeholder-img']} width="100%" height="100%" src="/static/images/heroes/secondary-3.png" />
-
-            <div className={`${styles['carousel-caption']} carousel-caption`}>
-              <div className='container'>
-                <Image className="d-block mx-auto" src="/static/logo/logo_with_brandname.png" alt="" width={236} height={64} />
-                <p>
-                  Tambahkan Kain Kinerja sofa & bagian menjadi ruang tamu Anda
-                </p>
-                <div className="d-grid gap-3 d-sm-flex justify-content-center">
-                  <Link href="/account">
-                    <a className="btn btn-primary px-4 me-sm-3">
-                      Tentang Kami
-                    </a>
-                  </Link>
-                  <Link href="/community">
-                    <a className="btn btn-outline-light px-4">
-                      Lihat Artikel
-                    </a>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={`${styles['carousel-item']} carousel-item`}>
-            <img className={styles['bd-placeholder-img']} width="100%" height="100%" src="/static/images/heroes/secondary-2.png" />
-
-            <div className={`${styles['carousel-caption']} carousel-caption text-end`}>
-              <div className='container'>
-                <Image className="d-block mx-auto" src="/static/logo/logo_with_brandname.png" alt="" width={236} height={64} />
-                <p>
-                  Kelembutan dan Warna menawarkan ketenangan dan relaksasi, mengurangi semua stres Anda.
-                </p>
-                <div className="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-end">
-                  <Link href="/account">
-                    <a className="btn btn-primary px-4 me-sm-3">
-                      Tentang Kami
-                    </a>
-                  </Link>
-                  <Link href="/community">
-                    <a className="btn btn-outline-light px-4">
-                      Lihat Artikel
-                    </a>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
+      <div className=''>
+        <img className={styles['bd-placeholder-img']} width="100%" height="100%" src="/static/images/heroes/main.png" />
       </div>
       <div className={`${styles['secondary-heroes']} text-bg-dark`}>
         <div className="px-4 py-5 text-center">
@@ -109,7 +46,9 @@ function Main() {
               Menerima pembuatan custom sofa dan service sofa berbagai model untuk rumah, kantor, apartement, hotel dan keperluan lainnya dengan biaya yang sangat bersaing.
             </p>
             <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-              <button type="button" className="btn btn-success btn-lg px-4 gap-3"><i className="bi bi-whatsapp"></i> Chat Kami</button>
+              <Button className="btn-lg px-4 gap-3" color="success" onClick={toggle}>
+                <i className="bi bi-whatsapp"></i> Chat Kami
+              </Button>
               <button type="button" className="btn btn-outline-light btn-lg px-4"><i className="bi bi-journal"></i> Lihat Layanan Kami</button>
             </div>
           </div>
@@ -142,34 +81,85 @@ function Main() {
         </div>
       </div>
 
-      <div className='container'>
-        <div className="row featurette">
-        <div className="col-md-7 py-2">
-          <div className='mb-2'>
-            <h2 className="featurette-heading fw-normal lh-1"><i className="pe-2 bi bi-caret-right-fill"></i>Jenis bahan yang kami gunakan</h2>
-            <p className="lead">Material pembuatan dan service sofa berkualitas, busa bersetifikasi tinggi internasional dan menggunakan kain yang berserat halus untuk menciptakan kenyamanan penggunanya.</p>
+      <div className="">
+        <div className="container card border-0 shadow rounded-3 overflow-hidden">
+          <div className="card-body p-0">
+            <div className="row gx-0">
+              <div className="col-lg-6 col-xl-5 py-lg-5">
+                <div className="p-4 p-md-5">
+                  <div className="badge bg-primary bg-gradient rounded-pill mb-2">News</div>
+                  <div className="h2 fw-bolder">Article heading goes here</div>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique delectus ab doloremque, qui doloribus ea officiis...</p>
+                  <a className="stretched-link text-decoration-none" href="#!">
+                    Read more
+                    <i className="bi bi-arrow-right"></i>
+                  </a>
+                </div>
+              </div>
+              <div className="col-lg-6 col-xl-7">
+                <div
+                  className={styles['bg-featured-blog']}
+                  style={{backgroundImage: "url(" + "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" + ")"}}>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className='mb-2'>
-            <h2 className="featurette-heading fw-normal lh-1"><i className="pe-2 bi bi-caret-right-fill"></i>Berbagai Macam Bentuk Sofa</h2>
-            <p className="lead">Kami dapat melakukan service pada sofa dengan berbagai macam bentuk sebagai berikut:</p>
-            <ul className="text-muted">
-              <li>Sofa 3.2.1</li>
-              <li>Sofa sudut L</li>
-              <li>Sofa minimalis</li>
-              <li>Sofa bed</li>
-              <li>Sofa Jaguar</li>
-              <li>Sofa Syahrini</li>
-              <li>Dan lain-lain</li>
-            </ul>
-          </div>
-        </div>
-        <div className="col-md-5">
-        <img className={`${styles['bd-placeholder-img']} mx-auto rounded-top`} width="100%" height="500" src="/static/images/heroes/main.png" />
         </div>
       </div>
-    </div>
+
+      <div className='container'>
+        <div className="mt-5 row featurette">
+          <div className="col-md-7 py-2">
+            <div className='mb-2'>
+              <h2 className="featurette-heading fw-normal lh-1"><i className="pe-2 bi bi-caret-right-fill"></i>Jenis bahan yang kami gunakan</h2>
+              <p className="lead">Material pembuatan dan service sofa berkualitas, busa bersetifikasi tinggi internasional dan menggunakan kain yang berserat halus untuk menciptakan kenyamanan penggunanya.</p>
+            </div>
+            <div className='mb-2'>
+              <h2 className="featurette-heading fw-normal lh-1"><i className="pe-2 bi bi-caret-right-fill"></i>Berbagai Macam Bentuk Sofa</h2>
+              <p className="lead">Kami dapat melakukan service pada sofa dengan berbagai macam bentuk sebagai berikut:</p>
+              <ul className="text-muted">
+                <li>Sofa 3.2.1</li>
+                <li>Sofa sudut L</li>
+                <li>Sofa minimalis</li>
+                <li>Sofa bed</li>
+                <li>Sofa Jaguar</li>
+                <li>Sofa Syahrini</li>
+                <li>Dan lain-lain</li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-md-5">
+            <img className={`${styles['bd-placeholder-img']} mx-auto rounded-top`} width="100%" height="500" src="/static/images/wall-1.png" />
+          </div>
+        </div>
+      </div>
     </Layout>
-  )
+    <Modal
+      isOpen={modal}
+      toggle={toggle}
+      backdrop={backdrop}
+      keyboard={keyboard}
+    >
+      <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+      <ModalBody>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+        culpa qui officia deserunt mollit anim id est laborum.
+      </ModalBody>
+      <ModalFooter>
+        <Button color="primary" onClick={toggle}>
+          Do Something
+        </Button>{' '}
+        <Button color="secondary" onClick={toggle}>
+          Cancel
+        </Button>
+      </ModalFooter>
+    </Modal>
+  </>)
 }
 
 export default Main
