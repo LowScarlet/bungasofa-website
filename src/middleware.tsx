b6ba6e0d4f40dd0fc3b13cc99d2a532e0098b16c
Low_Scarlet <namaetohyaakiradesu@gmail.com>
2022-09-19 10:59:56 +0700

    !

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -4,13 +4,14 @@ import { instagram, whatsapp } from './components/link_packs'
 export function middleware(request:any) {
     if (process.env.NODE_ENV === 'production') {
         const prod_domain = 'bungasofa.id'
+        if (request.nextUrl.hostname === prod_domain) {
+            return NextResponse.redirect(new URL('https://bungasofa.id', request.url))
+        }
         const sub = request.nextUrl.hostname.replace(prod_domain, '')
         if (sub === 'wa.') {
             return NextResponse.redirect(new URL(whatsapp, request.url))
         } else if (sub === 'instagram.') {
             return NextResponse.redirect(new URL(instagram, request.url))
-        } else {
-            return NextResponse.redirect(new URL('https://bungasofa.id', request.url))
         }
     }
 }
