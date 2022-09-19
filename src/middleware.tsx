c68c278b08105a9ae87ca746c9da858e1048cc8c
Low_Scarlet <namaetohyaakiradesu@gmail.com>
2022-09-19 11:03:38 +0700

    !

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -2,16 +2,19 @@ import { NextResponse } from 'next/server'
 import { instagram, whatsapp } from './components/link_packs'
 
 export function middleware(request:any) {
+    // Wildcard Domain Manager by Middleware
     if (process.env.NODE_ENV === 'production') {
         const prod_domain = 'bungasofa.id'
-        if (request.nextUrl.hostname != prod_domain) {
-            return NextResponse.redirect(new URL('https://bungasofa.id', request.url))
-        }
+
         const sub = request.nextUrl.hostname.replace(prod_domain, '')
         if (sub === 'wa.') {
             return NextResponse.redirect(new URL(whatsapp, request.url))
         } else if (sub === 'instagram.') {
             return NextResponse.redirect(new URL(instagram, request.url))
+        }
+
+        if (request.nextUrl.hostname != prod_domain) {
+            return NextResponse.redirect(new URL('https://bungasofa.id', request.url))
         }
     }
 }
