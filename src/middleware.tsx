b9abcd4c4a8e86a6b4b419a4c61cc22b5a0e35f9
Low_Scarlet <namaetohyaakiradesu@gmail.com>
2022-09-19 11:01:21 +0700

    !

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -4,7 +4,7 @@ import { instagram, whatsapp } from './components/link_packs'
 export function middleware(request:any) {
     if (process.env.NODE_ENV === 'production') {
         const prod_domain = 'bungasofa.id'
-        if (request.nextUrl.hostname === prod_domain) {
+        if (request.nextUrl.hostname != prod_domain) {
             return NextResponse.redirect(new URL('https://bungasofa.id', request.url))
         }
         const sub = request.nextUrl.hostname.replace(prod_domain, '')
